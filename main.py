f62a94015718a77054e0f159fa9ac1bd3b7b0697
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2025-04-07 22:12:56 +0000

    fix: port connectivity issue while hosting

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from utils import save_files_to_bucket, fetch_latest_BNS
 from dotenv import load_dotenv
+import uvicorn
 import os
-load_dotenv()
 
+load_dotenv()
 app = FastAPI()
 
 # base url
@@ -17,3 +18,7 @@ async def fetch():
    URL = os.getenv("MHA_BNS_PAGE_URL")
    fetch_latest_BNS(URL) # fetches & downloads to temp folder
    save_files_to_bucket() # saves to bucket & delete the files
+
+if __name__ == "__main__":
+   port = int(os.environ.get("PORT", 8000))
+   uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
